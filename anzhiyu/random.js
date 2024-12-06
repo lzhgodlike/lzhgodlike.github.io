736628ca2c3895a24aaf142ed1d0721462cdb6bc
lzhgodlike <1225732228@qq.com>
2024-12-06 15:32:02 +0800

    Site updated: 2024-12-06 15:32:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/06/hello-world/","2024/12/06/My-New-Post/"];function toRandomPost(){
+var posts=["2024/12/06/My-New-Post/","2024/12/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
