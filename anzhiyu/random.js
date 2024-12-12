f1d7343200aff5229d5ca8f6c1d127726749495d
lzhgodlike <1225732228@qq.com>
2024-12-12 19:32:13 +0800

    Site updated: 2024-12-12 19:32:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/06/My-New-Post/","2024/12/06/hello-world/","2024/12/12/如何在cloudflare中创建永久免费节点/"];function toRandomPost(){
+var posts=["2024/12/12/如何在cloudflare中创建永久免费节点/","2024/12/06/My-New-Post/","2024/12/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
