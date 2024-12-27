2d5ff89f6d03849b171339894eaa2e20882966fc
lzhgodlike <1225732228@qq.com>
2024-12-27 16:17:30 +0800

    Site updated: 2024-12-27 16:17:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/25/24122501/","2024/12/22/24122201/","2024/12/27/24122701/","2024/12/22/OS/","2024/12/12/CFjiedian/"];function toRandomPost(){
+var posts=["2024/12/25/24122501/","2024/12/27/24122701/","2024/12/22/24122201/","2024/12/22/OS/","2024/12/12/CFjiedian/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
