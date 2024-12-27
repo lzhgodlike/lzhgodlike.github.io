4526f24ced7f767f1055896372b3bb20db9ab153
lzhgodlike <1225732228@qq.com>
2024-12-27 14:53:35 +0800

    Site updated: 2024-12-27 14:53:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/22/24122201/","2024/12/12/CFjiedian/","2024/12/22/OS/"];function toRandomPost(){
+var posts=["2024/12/25/24122501/","2024/12/22/24122201/","2024/12/27/24122701/","2024/12/12/CFjiedian/","2024/12/22/OS/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
